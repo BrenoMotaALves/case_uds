80cbc64901158e49d25700c15357d1e8f087d909
BrenoMotaALves <bmotaa98@gmail.com>
2026-01-15 07:56:31 -0300

    fix(api): map domain errors to HTTP globally (APP_FILTER)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,17 @@ import { BoardsModule } from './modules/boards/boards.module';
 import { CardsModule } from './modules/cards/cards.module';
 import { ColumnsModule } from './modules/columns/columns.module';
 import { PrismaModule } from './shared/prisma/prisma.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionMapperFilter } from './shared/errors/http-exception.mapper';
 
 @Module({
   imports: [PrismaModule, BoardsModule, ColumnsModule, CardsModule],
-  controllers: [HealthController]
+  controllers: [HealthController],
+  providers:[
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionMapperFilter,
+    }
+  ]
 })
 export class AppModule {}
